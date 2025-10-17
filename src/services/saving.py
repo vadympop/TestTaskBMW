261cd274fd4d9d051e34ea2bd3008e9baeef48b4
Vadim Popovych <zyzel19@gmail.com>
2025-10-17 21:33:39 +0200

    fix(save_to_csv): update getting columns count

--- src/services/saving.py
+++ src/services/saving.py
@@ -18,18 +18,23 @@ def save_to_csv(
     :param results: list[AnswerResult]
     :return:
     """
-    # Get columns count
-    with open(csv_filename, "r", newline="") as csvfile:
-        reader = csv.reader(csvfile)
-        first_row = next(reader)
-        columns_count = len(first_row)
-
-    cur_pointer = 0
-    row = []
-
     # Add to the columns transcript text
     results.append(AnswerResult(answer=transcript, save_to_column=transcript_column))
     results.sort(key=lambda r: r.save_to_column)
+
+    # Get columns count
+    with open(csv_filename, "r", newline="") as csvfile:
+        reader = csv.reader(csvfile)
+        data = list(reader)  # Read all rows into a list
+        if data:  # Check if the list is not empty
+            last_row = data[-1]
+            columns_count = len(last_row)
+        else:
+            # If no one rows in file, use maximum value of save_to_column as columns_count
+            columns_count = max(results, key=lambda r: r.save_to_column).save_to_column
+
+    cur_pointer = 0
+    row = []
 
     # Add only values to row when it equals result save_to_column otherwise put empty string
     for i in range(columns_count):
